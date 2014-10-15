0dbbf431194f8ccc8e92098417a066d775855154
Nelson Chan <khchanel@gmail.com>
2014-10-15 16:33:36 +1100

    update location API

--- app/controllers/LocationController.php
+++ app/controllers/LocationController.php
@@ -9,7 +9,11 @@ class LocationController extends \BaseController {
      */
     public function index()
     {
-        $sr_locations = array('AIRL', 'BED1', 'HALL');
+        $sr_locations = array(
+                array('Code' => 1, 'Name' => 'AIRL', 'Description' => 'Air Lock'),
+                array('Code' => 2, 'Name' => 'BED1', 'Description' => 'Bedroom 1'),
+                array('Code' => 3, 'Name' => 'HALL', 'Description' => 'Hall'),
+            );
 
         return Response::json($sr_locations);
     }
